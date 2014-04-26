76794e662e135683f4710c311071c36e4e0c01a0
Stephen Jazdzewski <Steve@Jazd.com>
2014-04-26 08:56:26 -0700

    Working on individuals

--- PostgreSQL/post.sql
+++ PostgreSQL/post.sql
@@ -1,3 +1,8 @@
 -- Do not allow duplicate words based on case
 -- upper(value) is not allowed in an in-table constraint
 CREATE UNIQUE INDEX word_value ON word(culture,upper(value));
+
+-- Application or user insert on these tables should start at 10000
+-- This leaves room for global constants that are guaranteed to exist
+ALTER SEQUENCE word_id_seq RESTART WITH 10000;
+ALTER SEQUENCE sentence_id_seq RESTART WITH 10000;
