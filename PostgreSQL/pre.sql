2ec17224e4d63032fd820feecb205ad06d5912fe
Stephen Jazdzewski <Steve@Jazd.com>
2014-04-27 10:17:19 -0700

    Pass in a date to birthday functions, don't assume now()

--- PostgreSQL/pre.sql
+++ PostgreSQL/pre.sql
@@ -3,28 +3,28 @@ SET search_path TO Business,"$user",public;
 -- Functions
 
 -- Brute force birthday calculation
-CREATE OR REPLACE FUNCTION birthday(date)
+CREATE OR REPLACE FUNCTION birthday(birth date, asOf date)
  returns varchar(10)
 AS $$
 SELECT
-CASE WHEN extract(month FROM $1) < extract(month FROM NOW()) THEN
+CASE WHEN extract(month FROM birth) < extract(month FROM asOf) THEN
  -- past
-  (extract(year FROM NOW())::integer + 1) || '-' ||  extract(month FROM $1) || '-' ||  extract(day FROM $1)
+  (extract(year FROM asOf)::integer + 1) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
 ELSE
- CASE WHEN extract(month FROM $1) = extract(month FROM NOW())
-   AND extract(day FROM $1) < extract(day FROM NOW()) THEN
+ CASE WHEN extract(month FROM birth) = extract(month FROM asOf)
+   AND extract(day FROM birth) < extract(day FROM asOf) THEN
   -- past
-  (extract(year FROM NOW())::integer + 1) || '-' ||  extract(month FROM $1) || '-' ||  extract(day FROM $1)
+  (extract(year FROM asOf)::integer + 1) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
  ELSE
    -- future
-  extract(year FROM NOW()) || '-' ||  extract(month FROM $1) || '-' ||  extract(day FROM $1)
+  extract(year FROM asOf) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
  END
 END AS birthday
 $$ language sql immutable strict;
 
-CREATE OR REPLACE FUNCTION days_until_birthday(date)
+CREATE OR REPLACE FUNCTION days_until_birthday(birth date, asOf date)
  returns integer
 AS $$
  SELECT
- CAST(extract(days FROM CAST(birthday($1) AS date)-NOW()) AS integer) AS days
+ CAST(extract(days FROM CAST(birthday(birth) AS date) - CAST(asOf AS timestamp)) AS integer) AS days
 $$ language sql immutable strict;
