ee4354b881f4246f9039c02b40162a5fc22ac1ad
Stephen Jazdzewski <Steve@Jazd.com>
2014-04-26 22:37:36 -0700

    Doc

--- Static/Individual.sql
+++ Static/Individual.sql
@@ -1,5 +1,5 @@
 INSERT INTO Individual (person, goesBy, birth) VALUES (1, 'Steve', '1963-11-22');
-INSERT INTO Individual (person) VALUES (2);
-INSERT INTO Individual (person) VALUES (3);
-INSERT INTO Individual (person) VALUES (4);
+INSERT INTO Individual (person, birth) VALUES (2, '1986-01-31');
+INSERT INTO Individual (person, birth) VALUES (3, '1988-12-29');
+INSERT INTO Individual (person, birth) VALUES (4, '1982-10-28');
 INSERT INTO Individual (company, goesBy, birth) VALUES (1, 'IBM', '1911-06-16');
