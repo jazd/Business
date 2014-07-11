2438d64165ed7ded66afd2da3327726b3495c759
Stephen Jazdzewski <Steve@Jazd.com>
2014-07-10 17:36:03 -0700

    Untested lists

--- genDiagrams.sh
+++ genDiagrams.sh
@@ -6,12 +6,16 @@ mkdir -p diagrams
 ARGS="--gutter=50 --db=XML"
 
 INDIVIDUAL="Given Family Name Individual Company Word"
+LISTS="Individual IndividualList IndividualListName Word"
 
 # Include invalid refrences for display purposes only
 cat schema.xml | sed '/invalid/ {s/<comments invalid="">//; s/<\/comments>//}' > schema.xml.invalid
 
 scripts/extractTable.pl schema.xml.invalid $INDIVIDUAL >./zot.xml
 sqlt-diagram --title "Individual People and Company Events" $ARGS -c 2 -o diagrams/individual.png ./zot.xml
+
+scripts/extractTable.pl schema.xml.invalid $LISTS >./zot.xml
+sqlt-diagram --title "Lists of Individual People and Companies" $ARGS -c 2 -o diagrams/lists.png ./zot.xml
 
 # Remove temporary files
 rm zot.xml
