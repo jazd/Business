193959bf3b7aa42d1b6a308d85492b48ad0b5233
Stephen Jazdzewski <Steve@Jazd.com>
2017-12-17 14:34:53 -0800

    Person view to NuoDB
    Signed-off-by: Stephen Jazdzewski <Steve@Jazd.com>

--- NuoDB/pre.sql
+++ NuoDB/pre.sql
@@ -4,6 +4,82 @@
 -- http://doc.nuodb.com/Latest/Content/CREATE-FUNCTION.htm
 -- http://doc.nuodb.com/Latest/Content/CREATE-PROCEDURE.htm
 
+DROP FUNCTION IF EXISTS age;
+
+SET DELIMITER @
+CREATE FUNCTION age (
+ asOf DATE,
+ start DATE
+) RETURNS STRING AS
+ RETURN (
+  SELECT CAST(DATEDIFF(YEAR, start, asOf) AS STRING) || ' years' FROM DUAL
+ );
+END_FUNCTION;
+@
+SET DELIMITER ;
+
+SET DELIMITER @
+CREATE FUNCTION age (
+ start DATE
+) RETURNS STRING AS
+ RETURN (
+  SELECT CAST(DATEDIFF(YEAR, start, NOW()) AS STRING) || ' years' FROM DUAL
+ );
+END_FUNCTION;
+@
+SET DELIMITER ;
+
+
+DROP FUNCTION IF EXISTS birthday;
+
+SET DELIMITER @
+CREATE FUNCTION birthday (
+ birth DATE,
+ asOf DATE
+) RETURNS STRING AS
+ RETURN (
+  SELECT
+  CASE WHEN extract(month FROM birth) < extract(month FROM asOf) THEN
+    (extract(year FROM asOf) + 1) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
+  ELSE
+   CASE WHEN extract(month FROM birth) = extract(month FROM asOf)
+     AND extract(day FROM birth) < extract(day FROM asOf) THEN
+    (extract(year FROM asOf) + 1) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
+   ELSE
+    extract(year FROM asOf) || '-' ||  extract(month FROM birth) || '-' ||  extract(day FROM birth)
+   END
+  END AS birthday
+  FROM DUAL
+ );
+END_FUNCTION;
+@
+SET DELIMITER ;
+
+SET DELIMITER @
+CREATE FUNCTION birthday (
+ birth DATE
+) RETURNS STRING AS
+ RETURN (
+  SELECT birthday(birth, NOW()) FROM DUAL
+ );
+END_FUNCTION;
+@
+SET DELIMITER ;
+
+DROP FUNCTION IF EXISTS days_until_birthday;
+
+SET DELIMITER @
+CREATE FUNCTION days_until_birthday (
+ birth DATE,
+ asOf DATE
+) RETURNS INTEGER AS
+ RETURN (
+  SELECT DATEDIFF(DAY, asOf, birthday(birth)) FROM DUAL
+ );
+END_FUNCTION;
+@
+SET DELIMITER ;
+
 -- This should return the client culture state of the current connection or transaction
 -- For now it just returns 1033,'en-US'
 DROP FUNCTION IF EXISTS ClientCulture;
