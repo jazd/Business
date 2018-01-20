db5cde8b990b41bd6a50d2657728fad79444c8da
Stephen Jazdzewski <Steve@Jazd.com>
2018-01-20 11:43:26 -0800

    Make Session example more generic
    Signed-off-by: Stephen Jazdzewski <Steve@Jazd.com>

--- Sample/PostgreSQL/SQL/Session/Client.sql
+++ Sample/PostgreSQL/SQL/Session/Client.sql
@@ -1,4 +1,3 @@
--- PostgreSQL
 -- Client session example in two steps.
 -- 1)   A client hits the server without a session token, so generate an anonymous session to track it going forward.
 -- 1.a) Parse Client(Browser) Agent string.
@@ -18,7 +17,7 @@
 SELECT AnonymousSession('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
 'Chrome','43','0','2357','130','Linux',NULL,NULL,NULL,NULL,NULL,'Other',
 0,'www.ibm.com',NULL,NULL,
-'107.77.97.52');
+'107.77.97.52') FROM DUAL;
 --  anonymoussession 
 -- ------------------
 --                1  -- session id
@@ -26,7 +25,7 @@ SELECT AnonymousSession('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHT
 
 -- Step 2 for when session needs to be associated with a user. In this case, a new user(credential).
 -- 2.a) Assign a session token to the anonymous session
-INSERT INTO SessionToken (session, token, initialized, timeout, items) SELECT 1, 'BKrB9cYbZYcP1xKbKBOeXsAxDmoybyHn', 1, NULL, NULL;
+INSERT INTO SessionToken (session, token, initialized, timeout, items) SELECT 1, 'BKrB9cYbZYcP1xKbKBOeXsAxDmoybyHn', 1, NULL, NULL FROM DUAL;
 -- 2.b) Client generates a valid password
 --      No Provider or Generator, so password is in the clear
 INSERT INTO password (provider, generator, value) VALUES (NULL, NULL, '1234');
@@ -35,7 +34,8 @@ INSERT INTO password (provider, generator, value) VALUES (NULL, NULL, '1234');
 INSERT INTO credential (username, password, culture) VALUES ('helmet', 1, 1033);
 -- 2.d) Associate current session with new user credential
 --      (Token, siteId, agentStringId, credentialId, referringURLId, remoteAddr, locationId)
-SELECT SetSession('BKrB9cYbZYcP1xKbKBOeXsAxDmoybyHn', NULL, 1000, 1, 10, '107.77.97.52', NULL);
+--      Adds record to SessionCredential
+SELECT SetSession('BKrB9cYbZYcP1xKbKBOeXsAxDmoybyHn', NULL, 1000, 1, 10, '107.77.97.52', NULL) FROM DUAL;
 -- SetSession should be called on every page load to keep session alive and track the client
 
 
