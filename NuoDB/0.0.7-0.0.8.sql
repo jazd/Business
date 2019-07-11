c9ee011a37529585f713874aa9b3a8eabbc5efac
Stephen Jazdzewski <Steve@Jazd.com>
2019-07-11 09:06:38 -0700

    typeO

--- NuoDB/0.0.7-0.0.8.sql
+++ NuoDB/0.0.7-0.0.8.sql
@@ -1,4 +1,4 @@
 -- NuoDB database schema version 0.0.7 to 0.0.8
-CREATE INDEX periodname_name ON PeriodName (name);
+CREATE INDEX periodname_period ON PeriodName (period);
 CREATE INDEX AssemblyApplicationRelease_applicationRelease ON AssemblyApplicationRelease (applicationRelease);
 
