11ddd042c4ecab255e153438f0204aef11c4fcac
Stephen Jazdzewski <Steve@Jazd.com>
2020-03-12 08:08:44 -0700

    Basic Logging framework

---
+++ CSharp/Core.Test/TestFakeLog.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Business.Core.Test
+{
+	[TestFixture()]
+	public class TestFakeLog
+	{
+		private static Fake.Log log = new Fake.Log();
+		private const string message = "something";
+		private Exception exception;
+
+		[SetUp]
+		public void Init() {
+			exception = new Exception(message);
+		}
+
+		[Test]
+		public void Debug() {
+			log.Debug(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Debug.ToString()));
+			log.Debug(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Debug.ToString()));
+		}
+
+		[Test]
+		public void Info() {
+			log.Info(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Info.ToString()));
+			log.Info(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Info.ToString()));
+		}
+
+		[Test]
+		public void Warn() {
+			log.Warn(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Warn.ToString()));
+			log.Warn(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Warn.ToString()));
+		}
+
+		[Test]
+		public void
+			Error() {
+			log.Error(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Error.ToString()));
+			log.Error(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Error.ToString()));
+		}
+
+		[Test]
+		public void
+			Fatal() {
+			log.Fatal(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Fatal.ToString()));
+			log.Fatal(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Fatal.ToString()));
+		}
+
+		[Test]
+		public void
+			Trace() {
+			log.Trace(message);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Trace.ToString()));
+			log.Trace(exception);
+			Assert.That(log.Output, Contains.Substring(message));
+			Assert.That(log.Output, Contains.Substring(Log.Level.Trace.ToString()));
+		}
+	}
+}
---
+++ CSharp/Core/ILog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Business.Core
+{
+	public interface ILog
+	{
+		void Debug(string message);
+		void Debug(Exception ex);
+		void Info(string message);
+		void Info(Exception ex);
+		void Warn(string message);
+		void Warn(Exception ex);
+		void Error(string message);
+		void Error(Exception ex);
+		void Fatal(string message);
+		void Fatal(Exception ex);
+		void Trace(string message);
+		void Trace(Exception ex);
+	}
+}
