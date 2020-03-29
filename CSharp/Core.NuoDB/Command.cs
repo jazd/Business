f7b4e583fed3a92540078340b791333e8506f022
Stephen Jazdzewski <Steve@Jazd.com>
2020-03-29 04:34:34 -0700

    Book function now works in NuoDB and PostgreSQL

--- CSharp/Core.NuoDB/Command.cs
+++ CSharp/Core.NuoDB/Command.cs
@@ -7,14 +7,14 @@ namespace Business.Core.NuoDB
 	public class Command : ICommand
 	{
 		IReader Reader { get; set; }
-		public List<Parameter> Parameters { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
+		public List<Parameter> Parameters { get; set; }
 		public NuoDbConnection NuoDBClientConnection { get; set; }
 		public NuoDbCommand NuoDBClientCommand { get; set; }
 		public DbDataReader NuoDBReader { get; set; }
 
 		public Command() {
 			NuoDBClientCommand = new NuoDbCommand();
+			Parameters = new List<Parameter>();
 		}
 
 		public string CommandText {
@@ -27,14 +27,30 @@ namespace Business.Core.NuoDB
 		}
 
 		public IReader ExecuteReader() {
-			NuoDBClientCommand.Connection = NuoDBClientConnection;
+			MakeReady();
 			NuoDBReader = NuoDBClientCommand.ExecuteReader();
 			Reader = new Reader() { NuoDBReader = NuoDBReader };
 			return Reader;
 		}
 
 		public object ExecuteScalar() {
-			throw new System.NotImplementedException();
+			MakeReady();
+			return NuoDBClientCommand.ExecuteScalar();
+		}
+
+		private void MakeReady() {
+			NuoDBClientCommand.Connection = NuoDBClientConnection;
+			if (Parameters.Count > 0) {
+				// NuoDB uses ? for parameters in order, so new convert SQL Command Text
+				// TODO analize SQL to get actual order and repeats
+				// For now us super simple and not efficient assumint parameters are in order and no repeats
+				var sql = NuoDBClientCommand.CommandText;
+				foreach (var parameter in Parameters) {
+					sql = sql.Replace(parameter.Name, "?");
+					NuoDBClientCommand.Parameters.Add(parameter.Value);
+				}
+				NuoDBClientCommand.CommandText = sql;
+			}
 		}
 	}
 }
