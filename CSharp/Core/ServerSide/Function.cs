8f8532d349f53cf29029d9eaf0ae6e5c01d9e070
Stephen Jazdzewski <Steve@Jazd.com>
2020-04-01 07:27:29 -0700

    whitespace

--- CSharp/Core/ServerSide/Function.cs
+++ CSharp/Core/ServerSide/Function.cs
@@ -13,14 +13,8 @@ namespace Business.Core
 
 				using (var transaction = database.Connection.BeginTransaction(System.Data.IsolationLevel.ReadCommitted)) {
 					database.Command.CommandText = BookSQL;
-					database.Command.Parameters.Add(new Parameter() {
-						Name = "@book",
-						Value = name
-					});
-					database.Command.Parameters.Add(new Parameter() {
-						Name = "@amount",
-						Value = amount
-					});
+					database.Command.Parameters.Add(new Parameter() { Name = "@book", Value = name});
+					database.Command.Parameters.Add(new Parameter() { Name = "@amount", Value = amount});
 					entry = (UInt32?)(int?)database.Command.ExecuteScalar();
 
 					database.Connection.Commit();
