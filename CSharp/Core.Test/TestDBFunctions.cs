dc06d7730f36d06f1c3b7a9b39ee0186a1d90922
Stephen Jazdzewski <Steve@Jazd.com>
2020-04-02 10:17:19 -0700

    Simplify Tests

--- CSharp/Core.Test/TestDBFunctions.cs
+++ CSharp/Core.Test/TestDBFunctions.cs
@@ -4,8 +4,7 @@ using NUnit.Framework;
 namespace Business.Core.Test
 {
 	[TestFixture]
-	public class TestDBFunctions
-	{
+	public class TestDBFunctions {
 		[Test]
 		public void Book() {
 			var profile = new Profile.Profile();
@@ -19,10 +18,7 @@ namespace Business.Core.Test
 			database.SetValue(entryId);
 
 			var entry = database.Book(bookName, bookAmount);
-			Assert.IsTrue(database.Connection.TransactionStarted);
-			Assert.IsFalse(database.Connection.TransactionRollback);
-			Assert.IsTrue(database.Connection.TransactionCommited);
-			Assert.IsTrue(database.Connection.Closed);
+			CheckTransaction(database);
 
 			Assert.IsNotNull(entry);
 			Assert.Greater(entry, 0);
@@ -42,26 +38,14 @@ namespace Business.Core.Test
 			database.SetValue(entryId);
 
 			var entry = Function.SQLiteBook(database, bookName, bookAmount);
-			Assert.IsTrue(database.Connection.TransactionStarted);
-			Assert.IsFalse(database.Connection.TransactionRollback);
-			Assert.IsTrue(database.Connection.TransactionCommited);
-			Assert.IsTrue(database.Connection.Closed);
+			CheckTransaction(database);
 
 			Assert.IsNotNull(entry);
 			Assert.Greater(entry, 0);
 			Assert.AreEqual(entryId, entry);
 		}
 
-		[Test]
-		public void BookBalance() {
-			var profile = new Profile.Profile();
-			var database = new Fake.Database(profile);
-			database.Connect();
-
-			var bookName = "Sales";
-			float bookAmount = 111.11F;
-
-			var record1 = new object[] {
+		object[] record1 = new object[] {
 				(UInt32?)2,
 				(UInt32?)1,
 				(UInt32?)100,
@@ -73,7 +57,7 @@ namespace Business.Core.Test
 				100.00F,
 				DBNull.Value
 			};
-			var record2 = new object[] {
+		object[] record2 = new object[] {
 				(UInt32?)2,
 				(UInt32?)1,
 				(UInt32?)102,
@@ -86,14 +70,20 @@ namespace Business.Core.Test
 				100.00F
 			};
 
+		[Test]
+		public void BookBalance() {
+			var profile = new Profile.Profile();
+			var database = new Fake.Database(profile);
+			database.Connect();
+
+			var bookName = "Sales";
+			float bookAmount = 100.00F;
+
 			database.Add(record1);
 			database.Add(record2);
 
 			System.Collections.Generic.List<Balance> entries = database.BookBalance(bookName, bookAmount);
-			Assert.IsTrue(database.Connection.TransactionStarted);
-			Assert.IsFalse(database.Connection.TransactionRollback);
-			Assert.IsTrue(database.Connection.TransactionCommited);
-			Assert.IsTrue(database.Connection.Closed);
+			CheckTransaction(database);
 
 			Assert.Greater(entries.Count, 1);
 			Assert.IsNotNull(entries[0].Entry);
@@ -109,41 +99,13 @@ namespace Business.Core.Test
 			database.Connect();
 
 			var bookName = "Sales";
-			float bookAmount = 111.11F;
-
-			var record1 = new object[] {
-				(UInt32?)2,
-				(UInt32?)1,
-				(UInt32?)100,
-				(UInt32?)76,
-				"Cash",
-				false,
-				(UInt32?)70000,
-				"Asset",
-				100.00F,
-				DBNull.Value
-			};
-			var record2 = new object[] {
-				(UInt32?)2,
-				(UInt32?)1,
-				(UInt32?)102,
-				(UInt32?)112,
-				"Sales",
-				true,
-				(UInt32?)70003,
-				"Income",
-				DBNull.Value,
-				100.00F
-			};
+			float bookAmount = 100.00F;
 
 			database.Add(record1);
 			database.Add(record2);
 
 			System.Collections.Generic.List<Balance> entries = Function.SQLiteBookBalance(database, bookName, bookAmount);
-			Assert.IsTrue(database.Connection.TransactionStarted);
-			Assert.IsFalse(database.Connection.TransactionRollback);
-			Assert.IsTrue(database.Connection.TransactionCommited);
-			Assert.IsTrue(database.Connection.Closed);
+			CheckTransaction(database);
 
 			Assert.Greater(entries.Count, 1);
 			Assert.IsNotNull(entries[0].Entry);
@@ -153,6 +115,13 @@ namespace Business.Core.Test
 
 			Assert.AreEqual(1, database.Connection.OpenCount);
 			Assert.Less(database.Connection.CloseCount, 2);
+		}
+
+		public void CheckTransaction(Core.Fake.Database database) {
+			Assert.IsTrue(database.Connection.TransactionStarted);
+			Assert.IsFalse(database.Connection.TransactionRollback);
+			Assert.IsTrue(database.Connection.TransactionCommited);
+			Assert.IsTrue(database.Connection.Closed);
 		}
 	}
 }
