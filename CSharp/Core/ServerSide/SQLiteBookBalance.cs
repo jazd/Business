923737762f8ef75aaba352cd0666efb14d0f68bc
Stephen Jazdzewski <Steve@Jazd.com>
2020-04-02 07:08:42 -0700

    Failing BookBalanceSQLite test

---
+++ CSharp/Core/ServerSide/SQLiteBookBalance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Business.Core
+{
+	public partial class Function {
+		// Book single amounts into double entry Journal
+		public static List<Balance> SQLiteBookBalance(Core.IDatabase database, string name, float amount) {
+			List<Balance> result = new List<Balance>();
+
+			database.Connection.Close();
+			return result;
+		}
+	}
+}
